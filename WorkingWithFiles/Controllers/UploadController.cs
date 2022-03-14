872a6b253f8b3bdb936f8f14d4ac99457eeded54
romnkrki <romnkrki@gmail.com>
2022-03-14 14:34:06 +0545

    working with multiple files

--- WorkingWithFiles/Controllers/UploadController.cs
+++ WorkingWithFiles/Controllers/UploadController.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
@@ -11,7 +11,7 @@ namespace WorkingWithFiles.Controllers
     [ApiController]
     public class UploadController : ControllerBase
     {
-        [HttpPost("UploadFileAsyc")]
+        [HttpPost("UploadFileAsync")]
         public async Task<IActionResult> UploadAFileAsync(IFormFile file)
         {
             if (file.Length > 0)
@@ -29,5 +29,29 @@ namespace WorkingWithFiles.Controllers
             return Ok("No file detected to upload");
         }
 
+        [HttpPost("UploadMultipleFilesAsync")]
+        public async Task<IActionResult> UploadMultipleFilesAsync(List<IFormFile> files)
+        {
+            if (files != null)
+            {
+                foreach (var file in files)
+                {
+
+                    if (file.Length > 0)
+                    {
+                        var fileName = Path.GetFileName(file.FileName);
+
+                        var path = new PhysicalFileProvider(Path.Combine(Directory.GetCurrentDirectory(), "Uploads")).Root + $@"\{fileName}";
+
+                        using (var stream = System.IO.File.Create(path))
+                        {
+                            await file.CopyToAsync(stream);
+                        }
+                    }
+                }
+                return Ok("Done Suceesfully");
+            }
+            return Ok("Failed to upload your files");
+        }
     }
 }
